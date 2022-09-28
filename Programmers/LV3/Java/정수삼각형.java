0a767b9c2c9819a52839df5c9cafb978e8e7dc68
DragonJun <37258633+Golllum@users.noreply.github.com>
2022-09-29 02:09:01 +0900

    PRGMS_LV3_JAVA

---
+++ Programmers/LV3/Java/정수삼각형.java
@@ -0,0 +1,24 @@
+class Solution {    
+    public int solution(int[][] triangle) {
+        int answer = 0;
+        int[][] dp = new int[triangle.length][triangle[triangle.length-1].length];
+        
+        dp[0][0] = triangle[0][0];
+        
+        for(int i=1; i<triangle.length; i++){
+            dp[i][0] = dp[i-1][0] + triangle[i][0];
+        }
+        
+        for(int i=1; i<triangle.length; i++){
+            for(int j=1; j<triangle[i].length; j++){
+                dp[i][j] = Math.max(dp[i-1][j-1] + triangle[i][j], dp[i-1][j] + triangle[i][j]);
+            }
+        }
+        
+        for(int i=0; i<dp[0].length; i++){
+            answer = Math.max(dp[dp.length-1][i], answer);
+        }
+                
+        return answer;
+    }
+}
