f3aaab73aa04de0b5a971efc43d8661db9c14735
DragonJun <37258633+Golllum@users.noreply.github.com>
2022-10-10 22:50:06 +0900

    PRGMS_LV2

---
+++ Programmers/LV2/Javascript/멀리뛰기.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    // fibonacci 수열 => dp
+    let dp = [];
+    dp[1] = 1;
+    dp[2] = 2;
+    
+    for(let i=3; i<=n; i++){
+        dp[i] = (dp[i-2] + dp[i-1]) % 1234567;
+    }
+    
+    return dp[n];
+}
