f5014ff843df837d8d111b83536605b8f685db4c
DragonJun <37258633+Golllum@users.noreply.github.com>
2022-10-12 01:44:45 +0900

    PRGMS_LV2

---
+++ Programmers/LV2/Javascript/k진수에서소수갯수구하기.js
@@ -0,0 +1,18 @@
+function solution(n, k) {
+    return n.toString(k)
+            .split("0")
+            .filter(e => isPrime(e))
+            .length;
+}
+
+function isPrime(num){
+    if(!num || num < 2) return false;
+    
+    for(let i=2; i<=Math.sqrt(num); i++){
+        if(num % i == 0){
+            return false;
+        }
+    }
+    
+    return true;
+}
